cc5026dfa714935024556ac249a486638479c9a2
Abhimanyu Chakravarty <viperzenator@gmail.com>
2022-04-14 18:40:52 +0200

    Updated colors and icons

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ const data = require('./data.json');
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
 
+Handlebars.registerHelper("social", function(context, options) {
+    var html = "<tbody>";
+  
+    for (var i = 0, j = context.length; i < j; i++) {
+        if(i%2 == 0){
+            html = html + "<tr>";
+        }
+        html = html + "<td>" + options.fn(context[i]) + "</td>";
+        if(i%2 != 0){
+            html = html + "</tr>";
+        }
+    }
+    
+    return html + "</tbody>";
+  });
+
 readFile(path.resolve(__dirname, 'signature.handlebars'), 'utf-8').then(htmlTemplate => {
     const template = Handlebars.compile(htmlTemplate);
     const compiledTemplate = template(data);
